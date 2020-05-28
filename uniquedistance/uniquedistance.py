d2e7d82335af516a506702d6c88a8e0c40478a4c
Ian Dimayuga <git@iandimayuga.com>
2020-05-27 22:51:04 -0700

    Create Point class and distance_squared.

--- uniquedistance/uniquedistance.py
+++ uniquedistance/uniquedistance.py
@@ -1,1 +1,15 @@
 # https://think-maths.co.uk/uniquedistance
+class Point:
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+
+  def __eq__(self, other):
+    return self.x == other.x and self.y == other.y
+
+  def __lt__(self, other):
+    return self.y < other.y or (self.y == other.y and self.x < other.x)
+
+def distance_squared(point1, point2):
+  return (point2.x - point1.x)**2 + (point2.y - point1.y)**2
+
